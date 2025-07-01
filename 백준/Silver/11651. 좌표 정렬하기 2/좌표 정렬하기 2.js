d2a004f4f15c344b1f6130a48b48bac3aba1053a
Eunji Park <eunjipark0129@gmail.com>
2025-07-01 17:03:53 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 428 ms, Memory: 59256 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+function comp(a, b) {
+  if (a[1] !== b[1]) return a[1] - b[1];
+  return a[0] - b[0];
+}
+
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const points = input.slice(1).map((line) => line.split(' ').map(Number));
+points.sort(comp);
+
+const result = points.map((point) => point.join(' ')).join('\n');
+console.log(result);
